ab8d8d0b43caad991eed1fd6da638b7b7a803ed7
Cherif SALL <cherif.sall@outlook.com>
2019-03-31 18:15:21 +0200

    removing conflicting change

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-with open('README.rst') as file:
-    long_description = file.read()
+# with open('README.rst', encoding='utf-8') as file:
+#     long_description = file.read()
 
 setup(
     name='django-webp',
@@ -14,7 +14,7 @@ setup(
     url='http://pypi.python.org/pypi/django-webp/',
     license='MIT',
     description='Returns a webp image instead of jpg, gif or png to browsers which have support',
-    long_description=long_description,
+    long_description='see http://pypi.python.org/pypi/django-webp/',
     install_requires=open('requirements.txt').readlines(),
     include_package_data=True,
     zip_safe=False,
